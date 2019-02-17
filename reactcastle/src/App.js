b4d2035617245c079502c2138d9fb861b3534679
nicolas rouvet <nico.rouvet@gmail.com>
2019-02-17 23:24:22 +0100

    display2.0

--- reactcastle/src/App.js
+++ reactcastle/src/App.js
@@ -23,45 +23,57 @@ class App extends Component {
 
   render() {
     if (this.state.loading) {
-      return <p>Je suis en train de charger.</p>;
+      return <p>loading</p>;
     }
-    
 
     return (
       <html>
         <head>
-  <meta charset="utf-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-    crossorigin="anonymous" />
-  <link rel="stylesheet" href="style.css" />
-  <title>Casle</title>
+          <meta charset="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+            integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+            crossorigin="anonymous"
+          />
+          <link rel="stylesheet" href="style.css" />
+          <title>Casle</title>
         </head>
         <body>
-  <center>
-    <div class="container rounded" >
-      <p>
-        <h1>Liste des Hotels Restaurants étoilés</h1>
-      </p>
-      <p>
-        <div className ='wrapper'>
-        <ul>
-          {this.state.hotel.map((hotel) =>{
-            return(
-              <li key = {hotel.id}>
-              <h3>{hotel.hotelName}</h3>
-              <p>chef: {hotel.chef}</p>
-              <p>url : {hotel.url}</p>
-              </li>
-            )
-          })}
-          </ul>
-        </div>
-      </p>
-    </div>
-    </center>
-</body>
- 
+          <img
+            src="https://upload.wikimedia.org/wikipedia/fr/thumb/b/be/Logo_Relais_et_Chateaux.png/280px-Logo_Relais_et_Chateaux.png"
+            class="imgPriv"
+            alt="castle" 
+          />
+          <h1>Liste des Hotels Restaurants étoilés</h1>
+          <center>
+            <div>
+              <p>
+                <div className="wrapper">
+                  <ul>
+                    {this.state.hotel.map(hotel => {
+                      return (
+                        <section class="full-width">
+                          <div class="useful-width">
+                            <li key={hotel.id}>
+                              <h3>{hotel.hotelName}</h3>
+                              <p>chef: {hotel.chef}</p>
+                              <p> {hotel.url}</p>
+                            </li>
+                          </div>
+                        </section>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </p>
+            </div>
+          </center>
+        </body>
       </html>
     );
   }
